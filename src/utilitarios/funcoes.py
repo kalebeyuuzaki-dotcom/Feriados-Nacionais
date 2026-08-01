19d16e7c40a4e078b3397136ad10142a39398309
Hilton Kalebe Oliveira da Camara <kalebeyuuzaki@gmail.com>
2026-07-31 23:19:26 -0300

    Sem o .vscode

--- src/utilitarios/funcoes.py
+++ src/utilitarios/funcoes.py
@@ -1,22 +1,17 @@
 # Solicita ao usuário um número inteiro e valida a entrada até que seja aceita
 def pegar_ano():
     while True:
-        try:
-            # Pede a entrada como texto (string) primeiro
-            entrada = input('Informe o ano: ').strip()
+        # Pede a entrada como texto (string) primeiro
+        entrada = input('Informe o ano: ').strip()
 
-            # Verifica se possui 4 dígitos (Ex.: 2026) e se é somente números
-            if not entrada.isdigit() or len(entrada) != 4:
-                print('ERRO. Digite corretamente o ano. ')
-                continue
+        # Verifica se possui 4 dígitos (Ex.: 2026) e se é somente números
+        if not entrada.isdigit() or len(entrada) != 4:
+            print('ERRO. Digite corretamente o ano. ')
+            continue
 
-            # Se passou na validação, converte e retorna
-            ano = int(entrada)
-            return ano
-
-        except ValueError:
-            print('Tente novamente. ')
-
+        # Se passou na validação, converte e retorna
+        ano = int(entrada)
+        return ano
 
 # Área para testes
 if __name__ == '__main__':
